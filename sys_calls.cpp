73949f0e3558956bcf047e441692bc00bb7e32f2
Daniel Lind <dlind9@alaska.edu>
2018-02-06 10:51:39 -0900

    Stuff added

---
+++ sys_calls.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+
+using namespace std;
+
+int main()
+{
+    ssize_t chars=12;
+    write(1, "Hello World\n", chars);
+    write(1, "Hello again\n", chars);
+}
